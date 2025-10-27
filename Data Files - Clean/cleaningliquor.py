35274e04246b618e14758128d461df27f2c26821
Isaac Witrzens <isaac.witrzens@gmail.com>
2025-10-27 18:46:55 +1100

    final commit

--- Data Files - Clean/cleaningliquor.py
+++ Data Files - Clean/cleaningliquor.py
@@ -1,6 +1,5 @@
 import pandas as pd
 df = pd.read_csv(r"C:\Users\witrz\PycharmProjects\DATA1002\Data Files - Raw\LiqourLicenseRawData.csv")
-df = df[df["Licence type"] == "Liquor - packaged liquor licence"]
 #getting rid of unused columns
 df = df[["Licence type", "Suburb", "LGA"]]
 df.to_csv(r"C:\Users\witrz\PycharmProjects\DATA1002\Data Files - Clean\LiquorLicenseClean.csv", index=False)
